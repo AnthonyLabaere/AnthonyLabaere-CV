bf3031a71690726b51250a05e947376aa21ab962
Anthony Labaere <anthony.labaere@gmail.com>
2018-08-27 14:37:57 +0200

    Changement de style du menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router'; // <-- do not forget to import
+import { ActivatedRoute } from '@angular/router';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('appMenu', [
+      state(MenuComponent.MENU_HIDDEN_STATE, style({transform: 'translateX(0)'})),
+      transition(MenuComponent.MENU_HIDDEN_STATE + ' => ' + MenuComponent.MENU_DISPLAYED_STATE, [
+        style({transform: 'translateX(0)'}),
+        animate('250ms ease-out')
+      ]),
+      state(MenuComponent.MENU_DISPLAYED_STATE, style({transform: 'translateX(-150%)'})),
+      transition(MenuComponent.MENU_DISPLAYED_STATE + ' => ' + MenuComponent.MENU_HIDDEN_STATE, [
+        style({transform: 'translateX(-150%)'}),
+        animate('250ms ease-out')
+      ])
+    ])
+  ]
 })
 export class MenuComponent implements OnInit, AfterViewInit {
+  
+  public static MENU_HIDDEN_STATE = 'hidden';
+  public static MENU_DISPLAYED_STATE = 'displayed';
 
   public fragment = 'home';
-
-  public showMenu = false;
+  public showMenu = true;
 
   constructor(private route: ActivatedRoute) { }
 
